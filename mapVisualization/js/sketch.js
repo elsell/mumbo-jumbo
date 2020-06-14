0c87f1b872cd182852c7ca5234242424f7d4d657
elsell <john.sell@mail.wlc.edu>
2020-06-14 03:30:06 -0400

    Complete re-write of map Visualizer using Three.js

--- mapVisualization/js/sketch.js
+++ mapVisualization/js/sketch.js
@@ -1,3 +1,5 @@
+
+
 playerPos = []
 mapHeights = [];
 mapTrees = []
@@ -5,7 +7,7 @@ mapRivers = []
 mapEnemies = []
 mapSize = 0;
 DRAW_HEIGHTS = true
-CELL_SIZE = 20;
+CELL_SIZE = .01;
 HEIGHT_SCALAR = 5;
 COLORS = 
 [
@@ -35,6 +37,13 @@ FOLIAGE_COLORS =
     "#f0e68c", 
 ];
 
+TREES = []
+RIVERS = []
+ENEMIES = []
+PLAYERS = []
+
+REQUEST_SAVE = false
+
 let VIEWS = {
     "corner": "corner",
     "side": "side",
@@ -42,84 +51,200 @@ let VIEWS = {
 };
 let VIEW = 22;//VIEWS.corner;
 var c;
-function setup() {
-    height = 3500;
-    width = height * 1.77;
-    frameRate(20);
-    p5.disableFriendlyErrors = true; // disables Friendly errors, apparently more performant
+var inconsolata;
 
-    CreateViewSwitcher();
-    c = createCanvas(width,height, WEBGL);
-    debugMode(5000, 100, 0, 0, 0, 2000, 0, -1000, 0);
+
+var camera, scene, renderer, stats;
+var geometry, material, mesh;
+var controls
+
+init();
+animate();
+function animate()
+{
+    controls.update()
+    stats.update();
+    renderer.render( scene, camera );
+    
+    if(REQUEST_SAVE)
+    {
+        REQUEST_SAVE = false;
+        dataUrl = renderer.domElement.toDataURL("image/png").replace("image/png", "image/octet-stream");;
+        window.location.href = dataUrl
+    }
+
+    requestAnimationFrame( animate );
 }
 
-  
-var HAS_SAVE = false;
-function draw() {
-    orbitControl();
-    background("#e0e0e0");
-    push();            
-    
-    rotateY(PI/2);
-    translate((-mapSize * .50) * CELL_SIZE,0,(-mapSize * .50) * CELL_SIZE)
-    push();
-    switch(VIEW)
+function createStats() {
+    var stats = new Stats();
+    stats.setMode(0);
+
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.top = '250';
+    stats.domElement.style.left = null;
+    stats.domElement.style.right = '20';
+    stats.domElement.style.float = "right"
+
+    return stats;
+  }
+
+function ResetView()
+{
+
+    scene.position.set(0,0,0)
+    scene.translateX(CELL_SIZE*mapSize * -.5)
+    scene.translateZ(CELL_SIZE*mapSize * .5)
+
+    camera.position.set(
+        0,
+        mapSize * CELL_SIZE,
+        0
+    );
+    camera.lookAt(new THREE.Vector3
+    (
+        0,
+        0,
+        0
+    ));
+    camera.up.set(0,1,0)
+
+   camera.updateProjectionMatrix();
+}
+
+function init() {
+    canvases = document.body.getElementsByTagName("canvas");
+    if( canvases.length >= 1)
     {
-        case VIEWS.corner:
-
-            translate((-mapSize * .50) * CELL_SIZE,(-mapSize * .25) * CELL_SIZE,1000)
-            rotateX(-PI/6)
-            rotateY(PI/3)
-            translate((-mapSize * .55) * CELL_SIZE,0,200)
-
-            break;
-        case VIEWS.top:
-            rotateX(-PI/2);
-            translate((-mapSize * .50) * CELL_SIZE,-1900,(-mapSize * .50) * CELL_SIZE)
-            break;
-        case VIEWS.side:
-            translate((-mapSize * .50) * CELL_SIZE,(-mapSize * .25) * CELL_SIZE,1000)
-            rotateX(-PI/6)
-            break;
-        
+        for(var i = 0; i < canvases.length; i++)
+        {
+            canvases[i].remove()
+        }
     }
+
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 2;
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	scene = new THREE.Scene();
+    var alight = new THREE.AmbientLight(0xffffff,.3);
+    var light = new THREE.DirectionalLight(0xffffff,.7);
+    light.position.x = 0
+    light.position.y = 6
+    light.position.z = .1
+
+    scene.add(light)
+    scene.add(alight)
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true
+
+
+    stats = createStats()
+    document.body.appendChild( stats.domElement );
+
+    setupScene()
+
+}
+
+function ToggleRivers()
+{
+    for(var i = 0; i < RIVERS.length; i++)
+    {
+        RIVERS[i].visible = !RIVERS[i].visible;
+    }
+}
+
+function ToggleTrees()
+{
+    for(var i = 0; i < TREES.length; i++)
+    {
+        TREES[i].visible = !TREES[i].visible;
+    }
+}
+
+function ToggleEnemies()
+{
+    for(var i = 0; i < ENEMIES.length; i++)
+    {
+        ENEMIES[i].visible = !ENEMIES[i].visible;
+    }
+}
+
+function TogglePlayers()
+{
+    for(var i = 0; i < PLAYERS.length; i++)
+    {
+        PLAYERS[i].visible = !PLAYERS[i].visible;
+    }
+}
+
+function GetRiverLength(riverColumn)
+{
+    length = 0
+    for(i = 0; i < riverColumn.length; i++)
+    {
+        if( riverColumn[i] != 0)
+        {
+            length++;
+        }
+    }
+
+    return length
+}
+
+var HAS_SAVE = false;
+function setupScene() {
+
 
     for(var x = 0; x < mapSize; x++)
     {
-        push();
         for(var y = 0; y < mapSize; y++)
         {
-            push();
+            color = 0;
             var curCellHeight = mapHeights[x][y];
             if(x == 0 && y == 0)
             {
-                fill("#ff0000");
+                console.log("OK")
+                color = "#ff0000"
             }
             else
             {
-                colorStr = COLORS[int(curCellHeight)];
-                fill(colorStr);
+                color = COLORS[parseInt(curCellHeight)];
             }
 
             // Draw Terrain
-            stroke("#0e0e0e");
-            strokeWeight(1);
-            var boxHeight = curCellHeight * HEIGHT_SCALAR;
-            translate(y * CELL_SIZE, boxHeight * -.5,x * CELL_SIZE);
-            box(CELL_SIZE, boxHeight, CELL_SIZE);
+            var boxHeight = curCellHeight * HEIGHT_SCALAR + .001;
+    
+            geometry = new THREE.BoxBufferGeometry(CELL_SIZE,boxHeight,CELL_SIZE)
+            material = new THREE.MeshPhongMaterial({color:color})
+            mesh = new THREE.Mesh(geometry, material)
+            mesh.position.x = x * CELL_SIZE
+            mesh.position.y = boxHeight * .5
+            mesh.position.z = -y * CELL_SIZE
+            scene.add(mesh)
+
 
             // Draw Rivers
-            push();
             riverDirection = mapRivers[x][y];
+            
             if(riverDirection > 0)
             {
-                fill("#FF0000");
+                geometry = new THREE.BoxBufferGeometry(CELL_SIZE * .2,CELL_SIZE*.01,CELL_SIZE)
+                material = new THREE.MeshPhongMaterial()
+                mesh = new THREE.Mesh(geometry, material)
+                mesh.position.x = x * CELL_SIZE 
+                mesh.position.y = boxHeight
+                mesh.position.z = -y * CELL_SIZE
+
+                color = "#FF0000";
                 switch(riverDirection)
                 {
                     case 2:
                     case 4:
                         // East/West
-                        rotateY(PI/2);
+                        mesh.rotation.y = Math.PI / 2
                         break;
                     case 1:
                     case 3:
@@ -128,92 +253,113 @@ function draw() {
                     case 6:
                     case 7:
                         // NE/EN/SW/WS
-                        fill("#00ff00")
-                        translate(.10 * CELL_SIZE, 0,-.25 * CELL_SIZE);
-                        rotateY(-PI/4);
+                        color = "#00ff00"
+                        mesh.position.x -= .10 * CELL_SIZE
+                        mesh.position.z += -.25 * CELL_SIZE;
+                        mesh.rotation.y = -Math.PI/4
                         break;
                     case 16:
                     case 17:
                         // NE/EN/SW/WS
-                        translate(-.10 * CELL_SIZE, 0,.25 * CELL_SIZE);
-                        fill("#aaff00")
-                        rotateY(-PI/4);
+                        mesh.position.x += .10 * CELL_SIZE
+                        mesh.position.z -= -.25 * CELL_SIZE;
+                        color = "#aaff00"
+                        mesh.rotation.y = -Math.PI/4
                         
                         break;
                     case 5:
                     case 8:
                         // NW/WN/SE/ES
-                        fill("#0000ff")
-                        rotateY(PI/4);
-                        translate(.10 * CELL_SIZE, 0,.25 * CELL_SIZE);
+                        color = "#0000ff"
+                        mesh.rotation.y = Math.PI/4
+                        mesh.position.x += .10 * CELL_SIZE
+                        mesh.position.z += -.25 * CELL_SIZE;
                         break;
                     case 15:
                     case 18:
-                        translate(-.10 * CELL_SIZE, 0,-.25 * CELL_SIZE);
-                        fill("#00aaff")
-                        rotateY(PI/4);
+                        mesh.position.x -= .10 * CELL_SIZE
+                        mesh.position.z -= -.25 * CELL_SIZE;
+                        color = "#00aaff"
+                        mesh.rotation.y = Math.PI/4
                         break;
                     case 9:
                         // A puddle :P
-                        scale(.5,1,2.5);
-                        fill("#FF0000")
+                        mesh.scale.x = 2
+                        mesh.scale.z = .5
+                        color = "#FF0000"
                         break;
                     case 10:
                         // A river mouth ;D
-                        scale(.5,1,2.5);
-                        fill("#FF44AA");
+                        mesh.scale.x = 2
+                        mesh.scale.z = .5
+                        color = "#FF44AA"
                         break;
                     case 11:
                         // A swamp!
-                        scale(.5,1,2.5);
-                        fill("#5a6b16");
+                        mesh.scale.x = 2
+                        mesh.scale.z = .5
+                        color = "#5a6b16"
                         break;                        
 
                 }
-                box(CELL_SIZE + 1, boxHeight + 1 , CELL_SIZE * .2 + 1);
+                material.color = new THREE.Color(color)
+                scene.add(mesh)
+                RIVERS.push(mesh)
+
             }
-            pop();
+
             // Draw Trees
-            push();
-            var numTrees = int(mapTrees[x][y]);
+            var numTrees = parseInt(mapTrees[x][y]);
             if(numTrees > 0) 
             {
+                
                 var treeHeight = CELL_SIZE * numTrees * .7
-                translate(0,-boxHeight * .5 - treeHeight * .5 ,0);
-                translate(0,0,0);
     
-                colorStr = FOLIAGE_COLORS[int(numTrees)];
-                fill(colorStr);
-                if(DRAW_HEIGHTS)
-                    box(CELL_SIZE * .25,  treeHeight,CELL_SIZE * .25);
+                colorStr = FOLIAGE_COLORS[numTrees];
+                geometry = new THREE.BoxBufferGeometry(CELL_SIZE * .2,treeHeight,CELL_SIZE * .2)
+                material = new THREE.MeshPhongMaterial({color: colorStr})
+                mesh = new THREE.Mesh(geometry, material)
+                mesh.position.x = x * CELL_SIZE
+                mesh.position.y = boxHeight + treeHeight * .5
+                mesh.position.z = -y * CELL_SIZE
+
+                scene.add(mesh)
+                TREES.push(mesh)
             }
-            pop();
+
 
             // Draw enemies
-            push();
             var curEnemy = mapEnemies[x][y];
             if(curEnemy !== null)
             {
                 var treeHeight = CELL_SIZE * 15
-                translate(0,-boxHeight * .5 - treeHeight * .5 ,0);
-                translate(0,0,0);
-                
-                fill("#FF0000");
-                box(CELL_SIZE * .15,  treeHeight,CELL_SIZE * .15);
+                geometry = new THREE.BoxBufferGeometry(CELL_SIZE * .2,treeHeight,CELL_SIZE * .2)
+                material = new THREE.MeshPhongMaterial({color: "#ff0000"})
+                mesh = new THREE.Mesh(geometry, material)
+                mesh.position.x = x * CELL_SIZE- CELL_SIZE * .4
+                mesh.position.y = boxHeight + treeHeight * .5
+                mesh.position.z = -y * CELL_SIZE
+
+                scene.add(mesh)
+                ENEMIES.push(mesh)
             }
-            pop();
 
             // Draw Player Pos
-            push();
 
             try
             {
                 if(playerPos.x  == x && playerPos.y  == y)
                 {
                     var treeHeight = CELL_SIZE * 15
-                    translate(0,-boxHeight * .5 - treeHeight * .5 ,0);
-                    fill("#00FFFF");
-                    box(CELL_SIZE * .15,  treeHeight,CELL_SIZE * .15);
+                    geometry = new THREE.BoxBufferGeometry(CELL_SIZE * .2,treeHeight,CELL_SIZE * .2)
+                    material = new THREE.MeshPhongMaterial({color: "#00ffff"})
+                    mesh = new THREE.Mesh(geometry, material)
+                    mesh.position.x = x * CELL_SIZE - CELL_SIZE * .4
+                    mesh.position.y = boxHeight + treeHeight * .5
+                    mesh.position.z = -y * CELL_SIZE
+
+                    scene.add(mesh)
+                    PLAYERS.push(mesh)
                 }
             }catch(e)
             {
@@ -221,19 +367,12 @@ function draw() {
             }
             
             
-            pop();
-            pop();
         }
-        pop();
     }
-    pop();
-    CreateKey();
-    pop();
 }
 
 function LoadMap(data)
 {
-    clear();
     mapSize = data["heightMap"].length;
     mapHeights = data["heightMap"];
     mapTrees = data["treeMap"];
@@ -242,9 +381,10 @@ function LoadMap(data)
     playerPos = data["playerPosition"]
 
     // This ensures the map always fills the viewing window
-    CELL_SIZE = 1800 / mapSize;
     HEIGHT_SCALAR = CELL_SIZE * .25;
-    CreateKey();
+
+    init()
+    ResetView()
 }
 
 function CreateKey()
@@ -254,10 +394,8 @@ function CreateKey()
     for(color of COLORS)
     {
         push();
-        rotateZ(PI/9);     
-        rotateX(PI);
         fill(color)
-        square(-200, -500 + i++ * colorSize, colorSize)
+        square(-200, -1100 + i++ * colorSize, colorSize)
         pop();
     }
 }
@@ -278,7 +416,3 @@ function CreateViewSwitcher()
     }
 }
 
-function SaveImage()
-{
-    save(c, 'mapImage.png');
-}
---
+++ mapVisualization/js/zoom.js
@@ -0,0 +1,59 @@
+function zoom(event) {
+    // zoom according to direction of mouseWheelDeltaY rather than value
+    let sensitivityZoom = 0.05;
+    let scaleFactor = c.height;
+    if (event.deltaY > 0) {
+    c._curCamera._orbit(0, 0, sensitivityZoom * scaleFactor);
+    } else {
+    c._curCamera._orbit(0, 0, -sensitivityZoom * scaleFactor);
+    }
+    }
+
+document.onkeypress =(e) =>
+{
+    switch(e.key)
+    {
+        // vV
+        case "v":
+        case "V":
+            TogglePlayers()
+            break;
+        // tT 
+        case "t":
+        case "T":
+            ToggleTrees()
+            break;
+        // 
+        case "r":
+        case "R":
+            ToggleRivers()
+            break;
+        case "e":
+        case "E":
+            ToggleEnemies()
+            break;            
+        // space
+        case " ":
+            ResetView();
+            break;
+
+
+
+    }
+}
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+document.getElementById("save").onclick = () =>
+{
+    REQUEST_SAVE = true
+}
